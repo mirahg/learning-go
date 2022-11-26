045167eed14006a9bd7407e7fd98ec78f00e399c
mirahg <gordon.m@northeastern.edu>
2022-11-26 14:03:40 -0500

    finished 03_03

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	// instantiate array and then assign members at index
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+
+	// print all members of array
+	fmt.Println(colors)
+	// print member of array at index 0
+	fmt.Println(colors[0])
+
+	// instantiate array on same line
+	var numbers = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(numbers)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of numbers:", len(numbers))
 }
