762d2a06fae43fcbe9910a04e6f70dc7bd268fc2
mirahg <gordon.m@northeastern.edu>
2022-11-26 14:32:22 -0500

    finished 03_06

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	// structs similar to java/C but no inheritance
+
+	// create instance of struct
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	// change values
+	poodle.Weight = 9
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+}
+
+// create custom type
+// Dog is a struct
+type Dog struct { // exported/public
+	Breed  string
+	Weight int
 }
