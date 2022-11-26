71a0cfffaf351c2fdb35b9ff6cddb90a96074c7e
mirahg <gordon.m@northeastern.edu>
2022-11-26 14:15:07 -0500

    finished 03_04

--- practice/main.go
+++ practice/main.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	var colors = [3]string{"Red", "Green", "Blue"}
+	// slices
+	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	// append item to slice
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+
+	// remove item from slice
+	colors = append(colors[1:len(colors)]) // first item
+	fmt.Println(colors)
+
+	colors = append(colors[:len(colors)-1]) // last item
+	fmt.Println(colors)
+
+	// make -- allows you to append
+	numbers := make([]int, 5, 5) // array of ints, initial size 5, cap of 5
+	numbers[0] = 134
+	numbers[1] = 72
+	numbers[2] = 36
+	numbers[3] = 12
+	numbers[4] = 156
+	fmt.Println(numbers)
+
+	numbers = append(numbers, 235)
+	fmt.Println(numbers)
+
+	// sort array
+	sort.Ints(numbers)
+	fmt.Println(numbers)
+
 }
